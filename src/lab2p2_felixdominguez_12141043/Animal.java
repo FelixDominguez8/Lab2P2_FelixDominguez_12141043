f2a79f384f0e66d071c775fc99e36ad690716e74
StickyFingers84 <felixomardominguez847@gmail.com>
2022-01-28 14:32:43 -0600

    Commit 2

---
+++ src/lab2p2_felixdominguez_12141043/Animal.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_felixdominguez_12141043;
+
+/**
+ *
+ * @author Lenovo I7
+ */
+public class Animal {
+    private String nombrec;
+    private String nombre;
+    private String habitat;
+    private String alimentacion;
+    private String rasgos;
+    private String geo;
+    private int vida;
+    
+    public Animal(String nc, String n, String h, String a, String r, String g, int v){
+        
+    }
+    
+    public void setNombrec(String nc){
+        nombrec=nc;
+    }
+
+    public String getImei(){
+        return nombrec;
+    } 
+    
+    public void setNombre(String n){
+        nombre=n;
+    }
+
+    public String getNombre(){
+        return nombre;
+    } 
+    
+    public void setHabitat(String h){
+        habitat=h;
+    }
+
+    public String getHabitat(){
+        return habitat;
+    }
+    
+    public void setAlimentacion(String a){
+        alimentacion=a;
+    }
+
+    public String getAlimentacion(){
+        return alimentacion;
+    } 
+    
+    public void setRasgos(String r){
+        rasgos=r;
+    }
+
+    public String getRasgos(){
+        return rasgos;
+    } 
+    
+    public void setGeo(String nc){
+        nombrec=nc;
+    }
+
+    public String getImei(){
+        return nombrec;
+    } 
+}
--- src/lab2p2_felixdominguez_12141043/Lab2P2_FelixDominguez_12141043.java
+++ src/lab2p2_felixdominguez_12141043/Lab2P2_FelixDominguez_12141043.java
@@ -4,18 +4,54 @@
  * and open the template in the editor.
  */
 package lab2p2_felixdominguez_12141043;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author Lenovo I7
  */
 public class Lab2P2_FelixDominguez_12141043 {
+    static Scanner lea=new Scanner(System.in);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList animales=new ArrayList();
+        char resp='s';
+        while(resp=='s' || resp=='S'){
+            System.out.println();
+            System.out.println("- MENU -");
+            System.out.println("1) Registrar un animal");
+            System.out.println("2) Modificar un animal");
+            System.out.println("3) Eliminar un animal");
+            System.out.println("4) Listar animales");
+            System.out.println("5) Alimentar a un animal");
+            System.out.println("6) Salida");
+            System.out.println("Ingrese la opcion que usted desea: ");
+            int opcion=lea.nextInt();
+            System.out.println();
+            switch(opcion){
+                case 1:{
+                    break;
+                }
+                case 2:{
+                    break;
+                }
+                case 3:{
+                    break;
+                }
+                case 4:{
+                    break;
+                }
+                case 5:{
+                    break;
+                }
+                case 6:{
+                    break;
+                }
+            }
+        }
     }
-    
 }
